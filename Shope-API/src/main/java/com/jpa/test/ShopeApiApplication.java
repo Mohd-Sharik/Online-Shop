4604f88d2b91a1617d18b8d15daff4da1712a0e8
Mohd Sharik <sharikkhan9012@gmail.com>
2022-08-02 13:49:17 +0530

    add project folder

---
+++ Shope-API/src/main/java/com/jpa/test/ShopeApiApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeApiApplication.class, args);
+	}
+
+}
