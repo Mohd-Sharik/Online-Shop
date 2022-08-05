2835d5f83917532fb3cb3f65fb5d89ff3a1c227b
Mohd Sharik <sharikkhan9012@gmail.com>
2022-08-05 16:28:30 +0530

    add Data response Code with Paginatio and errorParameter and add some depandancy in pom.xml

--- shopCommon/src/main/java/commonModel/DatabaseOperationResult.java
+++ shopCommon/src/main/java/commonModel/DatabaseOperationResult.java
@@ -1,5 +1,73 @@
 package commonModel;
 
-public class DatabaseOperationResult {
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class DatabaseOperationResult<T> {
+	
+	
+	private T response;
+	private PaginationAttribute pageAttribute;
+	private ErrorParameter errorParam;
+	private Boolean sucess;
+	
+	
+	public DatabaseOperationResult()
+	{
+		pageAttribute = new PaginationAttribute();
+		errorParam = new ErrorParameter();
+	}
+
+
+	public T getResponse() {
+		return response;
+	}
+
+
+	public void setResponse(T response) {
+		this.response = response;
+	}
+
+
+	public PaginationAttribute getPageAttribute() {
+		return pageAttribute;
+	}
+
+
+	public void setPageAttribute(PaginationAttribute pageAttribute) {
+		this.pageAttribute = pageAttribute;
+	}
+
+
+	public ErrorParameter getErrorParam() {
+		return errorParam;
+	}
+
+
+	public void setErrorParam(ErrorParameter errorParam) {
+		this.errorParam = errorParam;
+	}
+
+
+	public Boolean getSucess() {
+		
+		this.sucess = StringUtils.isNoneEmpty(errorParam.getErrDesc()) ? true : false;
+		return sucess;
+	}
+
+
+	public void setSucess(Boolean sucess) {
+		this.sucess = sucess;
+	}
+
+
+	@Override
+	public String toString() {
+		getSucess();
+		return  ToStringBuilder.reflectionToString(this);
+	}
+	
+	
+	
 
 }
