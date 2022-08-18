3ad253236b554ad240014ae18a0d9c20b14c00a2
Mohd Sharik <sharikkhan9012@gmail.com>
2022-08-18 12:20:47 +0530

    add some method conversion of object in validation class

--- validationShop/src/main/java/validationUtils/validationUtils.java
+++ validationShop/src/main/java/validationUtils/validationUtils.java
@@ -13,7 +13,7 @@ public class validationUtils {
 		
 	}
 	
-	public static boolean isNull(Object object)
+	public static boolean isNull(final Object object)
 	{
 		if(object == null)
 		{
@@ -22,13 +22,72 @@ public class validationUtils {
 		return false;
 	}
 	
-	public static boolean isEmpty(Object object)
+	public static boolean isEmpty(final Object object)
 	{
 		if(object == null || "".equals(object))
 		{
 			return true;
 		}
 		return false;
+	}
+
+	public static boolean isValidSize(final Object object, final int size)
+	{
+		return object != null && object.toString().length() == size;
+	}
+	
+	public static boolean isBlowinLimit(final Object object, final int sizeLimit)
+	{
+		return object != null && object.toString().length() <= sizeLimit;
+	}
+	
+	public static boolean isNumaricBlowLimit(Object object, int limitSize)
+	{
+		return getValueBeforePreciasion(object).length() <= limitSize;
+	}
+	
+	public static String getValueBeforePreciasion(Object object)
+	{
+		if(object != null)
+		{
+				try
+				{
+					if(NumberUtils.isNumaric(object))
+					{
+						String s = object.toString();
+						if(s.contains("."))
+						{
+							s = s.substring(0, s.indexOf("."));
+						}
+						s = s.replaceAll("[+-]", "");
+						return s;
+					}
+				}
+		catch (Exception e) {
+			// TODO: handle exception
+			return "";
+		}
+	}
+		return "";
+	}
+	
+	public static class NumberUtils
+	{
+		public static boolean isNumaric(Object object)
+		{
+			if(object == null)
+			{
+				return false;
+			}
+			try
+			{
+				Double.parseDouble(object.toString());
+			}catch (Exception e) {
+				// TODO: handle exception
+				return false;
+			}
+			return true;
+		}
 	}
 
 }
